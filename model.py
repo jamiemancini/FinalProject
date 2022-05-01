0f3405b3488343fd08cb5a2a8839ea2997f6cf60
Jamie Mancini <jamiemancini@gmail.com>
2022-05-01 00:20:34 +0000

    seeded tables

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
 """Models for Final Project NameTBD"""
 
-from flask_sqalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-db = SQAlchemy()
+db = SQLAlchemy()
 
 class User(db.Model):
     """A user"""
@@ -16,8 +16,8 @@ class User(db.Model):
     password=db.Column(db.String)
     
     def __repr__(self):
-        # return f"<User user_id={self.user_id} first_name={self.first_name} last_name={self.last_name} email={self.email} password={sellf.passwprd}>"
-        #not sure what to keep
+        return f"<User user_id={self.user_id} first_name={self.first_name} last_name={self.last_name}>"
+
 
 class CampingSite(db.Model):
     """Campsites in CA State Parks and NPS"""
@@ -30,11 +30,38 @@ class CampingSite(db.Model):
     park_type=db.Column(db.String)
 
     def __repr__(self):
-        return f"<CampingSite camping_id={self.camping_id} "
+        return f"<CampingSite camping_id={self.camping_id}  park_name={self.park_name}>"
 
 class Rating(db.Model):
     """User Rating of Campsites"""
 
     __tablename__="ratings"
 
-    user_id=
+    rating_id=db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id= db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    camping_id=db.Column(db.Integer, db.ForeignKey("campsites.camping_id"))
+    review_description=db.Column(db.Text)
+    review_score=db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Rating user_id={self.user_id} camping_id{self.camping_id}> "
+
+def connect_to_db(flask_app, db_uri="postgresql:///wecamp", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
